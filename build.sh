2b5c7faf670e0ad40ce6e7842dc7424e4df0a395
khoitd1997 <khoidinhtrinh@gmail.com>
2022-02-08 10:27:06 -0800

    added base local conf

--- build.sh
+++ build.sh
@@ -5,7 +5,9 @@ script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 set -e
 
 windriver_linux_project_dir="${script_dir}/windriver_linux_project"
+conf_dir="${script_dir}/conf"
 wrlinux_dir="${windriver_linux_project_dir}/wrlinux-x"
+build_conf_dir="${windriver_linux_project_dir}/build/conf"
 
 mkdir -p "${windriver_linux_project_dir}"
 if [ ! -e "${wrlinux_dir}" ]; then
@@ -20,5 +22,6 @@ cd "${windriver_linux_project_dir}"
     --layers meta-networking meta-mingw meta-python meta-oe \
     --templates feature/lttng feature/gdb feature/sftp-server feature/wrvscode feature/lttng
 
-# source "${script_dir}/oe-init-build-env"
-# bitbake wrlinux-image-small
+source "${windriver_linux_project_dir}/oe-init-build-env"
+ln -sfv "${conf_dir}/local.conf" "${build_conf_dir}"
+bitbake wrlinux-image-small
